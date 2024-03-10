a8653bfed837ee881c48f9e49c9a8c599ab9efa4
mohit-sultania-07 <162908198+mohit-sultania-07@users.noreply.github.com>
2024-03-11 00:08:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector("button").addEventListener("click",function(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomGen= "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src",randomGen);
+  
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomGen2= "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src",randomGen2);
+  
+  
+  if(randomNumber1==randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+  else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  }
+  else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  }
+  
+})
+
